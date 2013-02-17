3cc8715581935dfed9cf7907afc04f9c87d6e81f
dtk <dtk@gmx.de>
2013-02-17 11:28:11 +0100

    Make activity invisible
    This commit sets a theme that doesn't display and removes the subsequently
    unneeded, unset layout. Output is logged rather that displayed in the UI.

--- src/de/dtk/anconpar/ParserActivity.java
+++ src/de/dtk/anconpar/ParserActivity.java
@@ -3,34 +3,31 @@ package de.dtk.anconpar;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.widget.TextView;
 
 public class ParserActivity extends Activity {
+
+	private static final String CLASS = ParserActivity.class.getSimpleName();
 
 	private static final String QUERY_PREFIX = "q=";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.layout_parser);
 		
 		Uri data = getIntent().getData();
 		
 		if (data != null) {
-			TextView mainText = (TextView)findViewById(R.id.main_text);
 			String query = data.getQuery();
 			if (query.startsWith(QUERY_PREFIX)) {
 				String address = query.substring(QUERY_PREFIX.length()).trim();
-				mainText.setText(address);
+				Log.d(CLASS, String.format("Address: %s", address));
 			} else {
-				mainText.setText(String.format("Error: Found invalid query: %s", query));
+				Log.e(CLASS, String.format("Error: Found invalid query: %s", query));
 			}
-			
-			
-			
 		}
-	}
+}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
